863471e00d393afe7119a3220df6f4c24e23fd0b
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-05-11 20:18:35 +0500

    Add files via upload

---
+++ 01 Number Guessing Game/Game.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guess a number beetween 1-5: ",
+  },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+  console.log("Congratulation! you guessed right number.");
+} else {
+  console.log("You guessed wrong number.");
+}
