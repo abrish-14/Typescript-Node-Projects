3def50ff5911960b2fd6e05e2e282d5a8d5a23b6
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-05-11 20:15:28 +0500

    Add files via upload

---
+++ 00 Simple Calculater/Calculater.ts
@@ -0,0 +1,42 @@
+//==#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+//Printing a welcome messege:
+console.log("/n/twelcome to/'codewithabrish/'-CLT simple calculater /n ");
+
+//Asking Questions From User Thought Inquirer:
+
+let answers = await inquirer.prompt([
+    {messege:"Enter first numbwer", type: "number",name:"firstNumber" },
+    {messege:"Enter second number", type: "number",name:"secondNumber"},
+    {
+        messege:"Select one operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication","Dvision"]
+     
+    },
+
+
+]);
+console.log(answers);
+
+// conditional statements IF-ELSE:
+
+if(answers.operator === "Addition"){
+ console.log(answers.firstNumber + answers.secondNumber) 
+}
+ else if (answers.operator === "Subtraction"){
+  console.log(answers.firstNumber - answers.secondNumber) 
+ }
+
+ else if (answers.operator === "Multiplication"){
+     console.log(answers.firstNumber * answers.secondNumber) 
+    } 
+    else if (answers.operator === "Division"){
+     console.log(answers.firstNumber / answers.secondNumber) 
+    }
+     else{ 
+        console.log("Invalid Input")
+}
